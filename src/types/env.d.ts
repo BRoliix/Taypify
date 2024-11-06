53639f555e8bedd669696a8c857f2008e8b44333
BRoliix <162447225+BRoliix@users.noreply.github.com>
2024-11-07 01:34:15 +0400

    ss

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,8 +1,6 @@
 declare global {
     namespace NodeJS {
       interface ProcessEnv {
-        NEXT_PUBLIC_JWT_SECRET: string;
-        JWT_SECRET: string;
         DATABASE_URL: string;
         NODE_ENV: 'development' | 'production' | 'test';
       }
